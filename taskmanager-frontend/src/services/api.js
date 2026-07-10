fe4c475b2c862b6f902db27b486ab6cba015a1fb
Aanya Dubey <aanya2523@gmail.com>
2026-07-10 18:17:44 +0530

    Connect frontend to Railway backend

--- taskmanager-frontend/src/services/api.js
+++ taskmanager-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/tasks",
+  baseURL: "https://task-manager-production-523d.up.railway.app/api/tasks",
 });
 
 export default api;
